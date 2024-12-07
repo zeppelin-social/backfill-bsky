d49949a6dbd68b07035db87703f1248c140c1c16
futurGH <me@futuristick.ca>
2024-12-07 15:50:00 -0500

    Update pds rate limits directly via db

--- src/requestCrawlPdses.ts
+++ src/requestCrawlPdses.ts
@@ -1,15 +1,17 @@
+import { Client } from "pg";
 import { fetchPdses } from "./util/fetch.js";
 
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
 			BSKY_REPO_PROVIDER: string;
+			BSKY_DB_POSTGRES_URL: string;
 			BGS_ADMIN_KEY: string;
 		}
 	}
 }
 
-for (const envVar of ["BSKY_REPO_PROVIDER", "BGS_ADMIN_KEY"]) {
+for (const envVar of ["BSKY_REPO_PROVIDER", "BSKY_DB_POSTGRES_URL", "BGS_ADMIN_KEY"]) {
 	if (!process.env[envVar]) throw new Error(`Missing env var ${envVar}`);
 }
 
@@ -22,56 +24,49 @@ async function main() {
 		console.log("Requesting crawls...");
 		await Promise.all(pdses.map(async (url) => {
 			try {
-				const res = await fetch(`${ bgs }/admin/pds/requestCrawl`, {
+				const res = await fetch(`${bgs}/admin/pds/requestCrawl`, {
 					method: "POST",
 					headers: {
 						"Content-Type": "application/json",
-						Authorization: `Bearer ${ process.env.BGS_ADMIN_KEY }`,
+						Authorization: `Bearer ${process.env.BGS_ADMIN_KEY}`,
 					},
 					body: JSON.stringify({ hostname: "https://" + url.hostname }),
 				});
 				if (!res.ok) {
 					console.error(
-						`Error requesting crawl for ${ url.hostname }: ${ res.status } ${ res.statusText } — ${ await res
-							.json().then((r: any) => r?.error || "unknown error") }`,
+						`Error requesting crawl for ${url.hostname}: ${res.status} ${res.statusText} — ${await res
+							.json().then((r: any) => r?.error || "unknown error")}`,
 					);
 				}
 			} catch (err) {
-				console.error(`Network error requesting crawl for ${ url.hostname }: ${ err }`);
+				console.error(`Network error requesting crawl for ${url.hostname}: ${err}`);
 			}
 		}));
 		console.log("Done crawling!");
 	}
-	
+
 	if (!process.argv.includes("--no-change-limits")) {
 		console.log("Setting rate limits...");
-		for (const url of pdses) {
 		try {
-			const limitsRes = await fetch(`${bgs}/admin/pds/changeLimits`, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-					Authorization: `Bearer ${process.env.BGS_ADMIN_KEY}`,
-				},
-				body: JSON.stringify({
-					host: url.hostname,
-					perSecond: 10_000,
-					perHour: 10_000_000,
-					perDay: 200_000_000,
-					repoLimit: 1_000_000,
-					crawlRate: 100_000,
-				}),
+			const db = new Client({
+				connectionString: process.env.BSKY_DB_POSTGRES_URL.replace("bsky", "bgs"),
 			});
-			if (!limitsRes.ok) {
-				console.error(
-					`Error setting rate limits for ${url.hostname}: ${limitsRes.status} ${limitsRes.statusText} — ${await limitsRes
-						.json().then((r: any) => r?.error || "unknown error")}`,
-				);
-			}
+
+			await db.query(
+				`
+				update pds set
+					rate_limit = $1,
+					crawl_rate_limit = $2,
+					repo_limit = $3,
+					hourly_event_limit = $4,
+					daily_event_limit = $5
+				where 1 = 1;
+			`,
+				[2000, 50, 1000000, 3000000, 20000000],
+			);
 		} catch (err) {
-			console.error(`Network error setting rate limits for ${url.hostname}: ${err}`);
+			console.error(`Error setting rate limits: ${err}`);
 		}
-	}
 		console.log("Done setting rate limits!");
 	}
 }
