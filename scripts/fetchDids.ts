d1c0b94d3f72c045fcc78855068361b62c9c2f4d
futurGH <me@futuristick.ca>
2025-05-20 19:33:42 -0400

    Maybe not creating so many listeners is a good idea

--- scripts/fetchDids.ts
+++ scripts/fetchDids.ts
@@ -5,23 +5,20 @@ import { fetchAllDids } from "../backfill/util/fetch.js";
 const stream = new EncoderStream();
 stream.pipe(createWriteStream("dids.cache"));
 
-let waitingForDrain = false;
-
-const count = await fetchAllDids(async (did, pds) => {
-	if (waitingForDrain) await waitForStreamDrain(stream);
-	if (!stream.write([did, pds])) {
-		waitingForDrain = true;
-	}
-});
-stream.end();
-stream.on("close", () => console.log(`Wrote ${count} DIDs to dids.cache`));
-
-async function waitForStreamDrain(stream: EncoderStream) {
-	if (!waitingForDrain) return;
+let drainPromise: Promise<void> | null = null;
+const createDrainPromise = () => {
 	return new Promise<void>((resolve) => {
-		stream.on("drain", () => {
-			waitingForDrain = false;
+		stream.once("drain", () => {
+			drainPromise = null;
 			resolve();
 		});
 	});
-}
+};
+
+const count = await fetchAllDids(async (did, pds) => {
+	if (drainPromise) await drainPromise;
+	if (!stream.write([did, pds])) drainPromise ??= createDrainPromise();
+});
+
+stream.end();
+stream.on("close", () => console.log(`Wrote ${count} DIDs to dids.cache`));
