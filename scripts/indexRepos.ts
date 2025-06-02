9afd516dedc85ad10c5798b7c3bada0fae229f1e
futurGH <me@futuristick.ca>
2025-06-02 09:30:15 -0400

    Add spidering and aggregates update to repo indexing script

--- scripts/indexRepos.ts
+++ scripts/indexRepos.ts
@@ -20,11 +20,23 @@ for (const envVar of ["BSKY_DB_POSTGRES_URL", "BSKY_DB_POSTGRES_SCHEMA", "BSKY_D
 	if (!process.env[envVar]) throw new Error(`Missing env var ${envVar}`);
 }
 
+const aggregatesUpdates: Array<Promise<void>> = [];
+
 async function main() {
+	const args = process.argv.slice(2);
+	let spider = false;
+	{
+		const spiderIndex = args.indexOf("--spider");
+		if (spiderIndex !== -1) {
+			args.splice(spiderIndex, 1);
+			spider = true;
+		}
+	}
+
 	const db = new Database({
 		url: process.env.BSKY_DB_POSTGRES_URL,
 		schema: process.env.BSKY_DB_POSTGRES_SCHEMA,
-		poolSize: 10,
+		poolSize: 100,
 	});
 
 	const idResolver = new IdResolver({
@@ -34,65 +46,106 @@ async function main() {
 
 	const indexingSvc = new IndexingService(db, idResolver, new BackgroundQueue(db));
 
-	await Promise.all(
-		process.argv.slice(2).map(async (did) => {
-			if (!did.startsWith("did:")) {
-				did = await idResolver.handle.resolve(did).then((r) => {
-					if (!r) throw new Error(`Invalid DID/handle: ${did}`);
-					return r;
-				});
-			}
-			return indexRepo(did, indexingSvc);
-		}),
-	);
+	await Promise.all(args.map(async (did) => {
+		if (!did.startsWith("did:")) {
+			did = await idResolver.handle.resolve(did).then((r) => {
+				if (!r) throw new Error(`Invalid DID/handle: ${did}`);
+				return r;
+			});
+		}
+		return indexRepo(did, indexingSvc, spider);
+	}));
+
+	await Promise.all(aggregatesUpdates)
+    .catch((error) => console.error("failed to update aggregates", error));
 }
 
-async function indexRepo(did: string, indexingSvc: IndexingService) {
-	const { idResolver } = indexingSvc;
-	const { pds } = await idResolver.did.resolveAtprotoData(did);
-	const agent = new XRPC({ handler: simpleFetchHandler({ service: pds }) });
-	const { data: repo } = await agent.get(`com.atproto.sync.getRepo`, {
-		params: { did: did as At.DID },
-	});
-
-	const commitData: Record<
-		string,
-		Array<{ did: string; path: string; cid: string; timestamp: string; obj: unknown }>
-	> = {};
-	const now = Date.now();
-	for await (const { record, rkey, collection, cid } of iterateAtpRepo(repo)) {
-		const path = `${collection}/${rkey}`;
-
-		let indexedAt: number =
-			(!!record && typeof record === "object" && "createdAt" in record
-				&& typeof record.createdAt === "string"
-				&& new Date(record.createdAt).getTime()) || 0;
-		if (!indexedAt || isNaN(indexedAt)) {
-			try {
-				indexedAt = parseTID(rkey).timestamp;
-			} catch {
-				indexedAt = now;
-			}
-		}
-		if (indexedAt > now) indexedAt = now;
-
-		const data = {
-			did,
-			path,
-			cid: cid.$link,
-			timestamp: new Date(indexedAt).toISOString(),
-			obj: record,
-		};
-
-		(commitData[collection] ??= []).push(data);
-	}
+async function indexRepo(did: string, indexingSvc: IndexingService, spider = false) {
+	const commitData = await getRepoRecords(did, indexingSvc);
+	if (Object.keys(commitData).length === 0) return;
 
 	const records = Object.values(commitData).flat();
 	const collections = new Map(Object.entries(commitData));
-	await Promise.all([
+
+	const promises = [
 		indexingSvc.bulkIndexToRecordTable(records),
 		indexingSvc.bulkIndexToCollectionSpecificTables(collections),
-	]);
+		indexingSvc.indexHandle(did, new Date().toISOString()),
+	];
+
+	const follows = collections.get("app.bsky.graph.follow") || [];
+	if (spider && follows.length) {
+		console.log(`spidering ${follows.length} follows for ${did}`);
+		promises.push(
+			...follows.map(({ obj }) => indexRepo((obj as any).subject, indexingSvc, false)),
+		);
+		aggregatesUpdates.push(
+			(indexingSvc.records.follow as any).params.updateAggregatesBulk?.(
+				indexingSvc.db.db,
+				follows.map(({ obj, did, cid, path, timestamp }) => ({
+					creator: did,
+					subjectDid: (obj as any).subject,
+					uri: `at://${did}/${path}`,
+					cid,
+					createdAt: timestamp,
+					indexedAt: timestamp,
+					sortAt: timestamp,
+				})),
+			),
+		);
+	}
+
+	await Promise.all(promises)
+	.catch((error) => {
+	  console.error(`failed to index repo for ${did}`, error);
+  })
+}
+
+async function getRepoRecords(did: string, indexingSvc: IndexingService) {
+	try {
+		const { idResolver } = indexingSvc;
+		const { pds } = await idResolver.did.resolveAtprotoData(did);
+		const agent = new XRPC({ handler: simpleFetchHandler({ service: pds }) });
+		const { data: repo } = await agent.get(`com.atproto.sync.getRepo`, {
+			params: { did: did as At.DID },
+		});
+
+		const records: Record<
+			string,
+			Array<{ did: string; path: string; cid: string; timestamp: string; obj: unknown }>
+		> = {};
+		const now = Date.now();
+		for await (const { record, rkey, collection, cid } of iterateAtpRepo(repo)) {
+			const path = `${collection}/${rkey}`;
+
+			let indexedAt: number =
+				(!!record && typeof record === "object" && "createdAt" in record
+					&& typeof record.createdAt === "string"
+					&& new Date(record.createdAt).getTime()) || 0;
+			if (!indexedAt || isNaN(indexedAt)) {
+				try {
+					indexedAt = parseTID(rkey).timestamp;
+				} catch {
+					indexedAt = now;
+				}
+			}
+			if (indexedAt > now) indexedAt = now;
+
+			const data = {
+				did,
+				path,
+				cid: cid.$link,
+				timestamp: new Date(indexedAt).toISOString(),
+				obj: record,
+			};
+
+			(records[collection] ??= []).push(data);
+		}
+		return records;
+	} catch (error) {
+		console.warn(`failed to index repo for ${did}`, error);
+		return {};
+	}
 }
 
 void main();
