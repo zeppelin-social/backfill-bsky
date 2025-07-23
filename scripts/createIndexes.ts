8fe728e8dd5b4845614fe39587307768d4fd1bcf
futurGH <me@futuristick.ca>
2025-07-23 10:16:17 -0400

    Set createIndexes pool size

--- scripts/createIndexes.ts
+++ scripts/createIndexes.ts
@@ -4,6 +4,7 @@ import { readFileSync } from "node:fs";
 const db = new Database({
 	url: process.env.BSKY_DB_POSTGRES_URL,
 	schema: process.env.BSKY_DB_POSTGRES_SCHEMA,
+	poolSize: 50,
 });
 
 const createCmds = readFileSync("create.sql", "utf-8").split("\n").map(cmd => `SET statement_timeout = 0; ${cmd}`);
